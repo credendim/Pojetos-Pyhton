6352e4956f48f5f85e6c61bd6b5e4f4581a03af9
Gabriel Credendio <gabrielcfurtunato@gmail.com>
2024-06-14 09:18:26 -0300

    first commit

---
+++ Aula1406/calculadora.py
@@ -0,0 +1,26 @@
+# Calculadora Simples
+
+while True:
+
+    print('\n\t\t\t -- Calculadora Simples -- \n')
+
+    print('1. Soma')
+    print('2. Sair')
+
+    op = int(input('\n\t Opção:'))
+
+    if op == 1:
+        print('\n\t\t\t -- Soma -- \n')
+
+        # Entrada
+
+        # Porcessamento
+
+        # Saída
+
+    elif op == 2:
+        print('\n\nForte Abraço \n')
+        break
+
+    else:
+        print('Opção {} incorreta' .format(op))
